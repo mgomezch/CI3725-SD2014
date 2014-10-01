cedd32cbda18bd32cba0145e3baf4172befe4fdd
Manuel Gómez <targen@gmail.com>
2014-10-01 14:04:35 -0430

    Asignación de evaluación de primera etapa lista

--- meta.sql
+++ meta.sql
@@ -1,5 +1,3 @@
-begin;
-
 -- Semilla seleccionada aleatoriamente:
 select setseed(0.42);
 
@@ -27,65 +25,89 @@ create table "Sección"
   )
 ;
 
-insert into "Sección"
-  ("número") values
-  (1       )
-, (2       )
-;
-
-
-
 create table "Estudiante"
   ( "carné"   char(7) not null
   , "nombre"  text    not null
-  , "sección" integer not null
   , primary key ("carné")
+  )
+;
+
+create table "Inscripción"
+  ( "estudiante" char(7) not null
+  , "sección"    integer not null
+  , primary key ("estudiante")
+  , foreign key ("estudiante") references "Estudiante" ("carné")
   , foreign key ("sección") references "Sección" ("número")
   )
 ;
 
-insert into "Estudiante"
-  ("carné"  , "nombre"                              , "sección") values
-  ('0436808', 'Arquímedes D Carrasquel Blanco'      ,         1)
-, ('0538956', 'Alexánder J Simoes Centeno'          ,         1)
-, ('0741206', 'Brian-Marcial Mendoza Bollam'        ,         1)
-, ('0741329', 'Dennis Esteban Pérez Camacho'        ,         1)
-, ('0741654', 'Nílver Fernando Viera Araujo'        ,         1)
-, ('0810398', 'Alejandro Jose Garbi Moya'           ,         1)
-, ('0810479', 'David Enrique Goudet Díaz'           ,         1)
-, ('0910123', 'David Gonzalo Caicedo Mora'          ,         1)
-, ('0910236', 'Andrea Carolina Díaz Mora'           ,         1)
-, ('0910329', 'María Gabriela Giménez Rodríguez'    ,         1)
-, ('0910430', 'Alessandro Augusto La Corte Romagni' ,         1)
-, ('0910672', 'Carlo Emigdio Polisano Sánchez'      ,         1)
-, ('0910797', 'Nelson Avelino Saturno Teles'        ,         1)
-, ('0911020', 'Luiscarlo Rivera Arias'              ,         1)
-, ('1010088', 'Stéfany Daniela Botero Mendoza'      ,         1)
-, ('1010231', 'Paolángela Espinal Useche'           ,         1)
-, ('1010445', 'Cristian Adrián Medina Villarroel'   ,         1)
-, ('1010539', 'Daniel Enrique Pelayo Useche'        ,         1)
-, ('1010757', 'Reinaldo Enrique Verdugo Chávez'     ,         1)
+with
+  "Datos de estudiantes" as (
+    select *
+    from
+      ( values
+        ('0436808', 'Arquímedes D Carrasquel Blanco'      ,         1)
+      , ('0538956', 'Alexánder J Simoes Centeno'          ,         1)
+      , ('0741206', 'Brian-Marcial Mendoza Bollam'        ,         1)
+      , ('0741329', 'Dennis Esteban Pérez Camacho'        ,         1)
+      , ('0741654', 'Nílver Fernando Viera Araujo'        ,         1)
+      , ('0810398', 'Alejandro Jose Garbi Moya'           ,         1)
+      , ('0810479', 'David Enrique Goudet Díaz'           ,         1)
+      , ('0910123', 'David Gonzalo Caicedo Mora'          ,         1)
+      , ('0910236', 'Andrea Carolina Díaz Mora'           ,         1)
+      , ('0910329', 'María Gabriela Giménez Rodríguez'    ,         1)
+      , ('0910430', 'Alessandro Augusto La Corte Romagni' ,         1)
+      , ('0910672', 'Carlo Emigdio Polisano Sánchez'      ,         1)
+      , ('0910797', 'Nelson Avelino Saturno Teles'        ,         1)
+      , ('0911020', 'Luiscarlo Rivera Arias'              ,         1)
+      , ('1010088', 'Stéfany Daniela Botero Mendoza'      ,         1)
+      , ('1010231', 'Paolángela Espinal Useche'           ,         1)
+      , ('1010445', 'Cristian Adrián Medina Villarroel'   ,         1)
+      , ('1010539', 'Daniel Enrique Pelayo Useche'        ,         1)
+      , ('1010757', 'Reinaldo Enrique Verdugo Chávez'     ,         1)
 
-, ('0538087', 'Víctor M De Ponte Olivares'          ,         2)
-, ('0740913', 'Ángel Roso Franco Carvajal'          ,         2)
-, ('0741249', 'Jónathan J Moreno Correa'            ,         2)
-, ('0810223', 'Susana Charara Charara'              ,         2)
-, ('0810462', 'Betzabeth Carolina González Canónico',         2)
-, ('0910029', 'Gabriel Armando Álvarez Ramírez'     ,         2)
-, ('0910219', 'José Ignacio Delgado Campos'         ,         2)
-, ('0910270', 'Carlos Adrián Farinha Couce'         ,         2)
-, ('0910381', 'Christhian Jesús Guevara Valencia'   ,         2)
-, ('0910502', 'Francisco Antonio Martínez Medina'   ,         2)
-, ('0910832', 'Víctor Julio Suniaga Martín'         ,         2)
-, ('0910971', 'Alejandro Jesús Guevara Allen'       ,         2)
-, ('0911207', 'Gustavo Enrique Siñóvsky Sánchez'    ,         2)
-, ('1010132', 'Fabio Alexánder Castro Martinez'     ,         2)
-, ('1010353', 'Andrés Rafael Hernández Monterola'   ,         2)
-, ('1010534', 'Jesús Adolfo Parra Parra'            ,         2)
-, ('1010608', 'Vanessa Eleonora Rívas Serio'        ,         2)
-, ('1010738', 'Abelardo Jesús Valiño Ovalle'        ,         2)
-, ('1010898', 'Jesús Humberto Sánchez Pérez'        ,         2)
-, ('1011247', 'Rosángelis Verónica García Dorta'    ,         2)
+      , ('0538087', 'Víctor M De Ponte Olivares'          ,         2)
+      , ('0740913', 'Ángel Roso Franco Carvajal'          ,         2)
+      , ('0741249', 'Jónathan J Moreno Correa'            ,         2)
+      , ('0810223', 'Susana Charara Charara'              ,         2)
+      , ('0810462', 'Betzabeth Carolina González Canónico',         2)
+      , ('0910029', 'Gabriel Armando Álvarez Ramírez'     ,         2)
+      , ('0910219', 'José Ignacio Delgado Campos'         ,         2)
+      , ('0910270', 'Carlos Adrián Farinha Couce'         ,         2)
+      , ('0910381', 'Christhian Jesús Guevara Valencia'   ,         2)
+      , ('0910502', 'Francisco Antonio Martínez Medina'   ,         2)
+      , ('0910832', 'Víctor Julio Suniaga Martín'         ,         2)
+      , ('0910971', 'Alejandro Jesús Guevara Allen'       ,         2)
+      , ('0911207', 'Gustavo Enrique Siñóvsky Sánchez'    ,         2)
+      , ('1010132', 'Fabio Alexánder Castro Martinez'     ,         2)
+      , ('1010353', 'Andrés Rafael Hernández Monterola'   ,         2)
+      , ('1010534', 'Jesús Adolfo Parra Parra'            ,         2)
+      , ('1010608', 'Vanessa Eleonora Rívas Serio'        ,         2)
+      , ('1010738', 'Abelardo Jesús Valiño Ovalle'        ,         2)
+      , ('1010898', 'Jesús Humberto Sánchez Pérez'        ,         2)
+      , ('1011247', 'Rosángelis Verónica García Dorta'    ,         2)
+
+      , ('0910794', 'Miguel  Saraiva'                     ,      NULL)
+      , ('0910894', 'Ricardo Vethencourt'                 ,      NULL)
+      , ('1010640', 'Donato Rolo'                         ,      NULL)
+      ) as "Datos de estudiantes"
+        ("carné"  , "nombre"                              , "sección")
+  ),
+  "Insertar secciones" as (
+    insert into "Sección" ("número")
+    select distinct "Datos de estudiantes"."sección"
+    from            "Datos de estudiantes"
+    where           "Datos de estudiantes"."sección" is not null
+  ),
+  "Insertar estudiantes" as (
+    insert into "Estudiante" ("carné", "nombre")
+    select "Datos de estudiantes"."carné", "Datos de estudiantes"."nombre"
+    from   "Datos de estudiantes"
+  )
+insert into "Inscripción" ("estudiante", "sección")
+select "Datos de estudiantes"."carné", "Datos de estudiantes"."sección"
+from   "Datos de estudiantes"
+where  "Datos de estudiantes"."sección" is not null
 ;
 
 
@@ -197,18 +219,18 @@ insert into     "Asignación de evaluación" ("grupo", "evaluación", "evaluador
 select distinct "Grupo"."número", "Grupo"."evaluación", 'Matteo Ferrando'
 from
   "Grupo"
-  inner join "Integrante" on true
-    and "Grupo"."número"     = "Integrante"."grupo"
-    and "Grupo"."evaluación" = "Integrante"."evaluación"
-    inner join "Estudiante" on true
-      and "Integrante"."estudiante" = "Estudiante"."carné"
-      and "Estudiante"."nombre" in
-        ( 'Reinaldo Enrique Verdugo Chávez'
-        , 'Vanessa Eleonora Rívas Serio'
-        )
-  inner join "Evaluación" on true
-    and "Grupo"."evaluación" = "Evaluación"."nombre"
-    and "Evaluación"."nombre" = 'Etapa 1 del proyecto'
+    inner join "Integrante" on true
+      and "Grupo"."número"     = "Integrante"."grupo"
+      and "Grupo"."evaluación" = "Integrante"."evaluación"
+      inner join "Estudiante" on true
+        and "Integrante"."estudiante" = "Estudiante"."carné"
+        and "Estudiante"."nombre" in
+          ( 'Reinaldo Enrique Verdugo Chávez'
+          , 'Vanessa Eleonora Rívas Serio'
+          )
+    inner join "Evaluación" on true
+      and "Grupo"."evaluación" = "Evaluación"."nombre"
+      and "Evaluación"."nombre" = 'Etapa 1 del proyecto'
 ;
 
 -- Los demás proyectos en Ruby son de David (entrega 1):
@@ -216,12 +238,12 @@ insert into "Asignación de evaluación" ("grupo", "evaluación", "evaluador")
 select "Grupo"."número", "Grupo"."evaluación", 'David Lilue'
 from
   "Grupo"
-  left outer join "Asignación de evaluación" on true
-    and "Grupo"."número"     = "Asignación de evaluación"."grupo"
-    and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
-  inner join "Evaluación" on true
-    and "Grupo"."evaluación" = "Evaluación"."nombre"
-    and "Evaluación"."nombre" = 'Etapa 1 del proyecto'
+    inner join "Evaluación" on true
+      and "Grupo"."evaluación" = "Evaluación"."nombre"
+      and "Evaluación"."nombre" = 'Etapa 1 del proyecto'
+    left outer join "Asignación de evaluación" on true
+      and "Grupo"."número"     = "Asignación de evaluación"."grupo"
+      and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
 where true
   and "Grupo"."evaluación" = 'Etapa 1 del proyecto'
   and "Grupo"."lenguaje"   = 'Ruby'
@@ -239,17 +261,73 @@ select
   end
 from
   "Grupo"
-  left outer join "Asignación de evaluación" on true
-    and "Grupo"."número"     = "Asignación de evaluación"."grupo"
-    and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
+    left outer join "Asignación de evaluación" on true
+      and "Grupo"."número"     = "Asignación de evaluación"."grupo"
+      and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
 where true
   and "Grupo"."evaluación" = 'Etapa 1 del proyecto'
   and "Grupo"."lenguaje"   = 'Haskell'
 ;
 
--- Los demás se distribuyen hasta que todos tengan más o menos la misma cantidad (entrega 1):
--- ???
+-- Los demás se distribuyen hasta que todos tengan más o menos la misma cantidad:
+create function "Asignar evaluación"("evaluación" text)
+returns void
+language "plpgsql"
+as $$
+  declare
+    "entrega" record;
+  begin
+    for "entrega" in
+      select
+        "Grupo"."número" as "grupo",
+        "Grupo"."evaluación"
+      from
+        "Grupo"
+          left outer join "Asignación de evaluación" on true
+            and "Grupo"."número"     = "Asignación de evaluación"."grupo"
+            and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
+      where true
+        and "Grupo"."evaluación" = $1
+        and "Asignación de evaluación"."evaluador" is null
+      order by
+        random()
+    loop
+      insert into "Asignación de evaluación" ("grupo", "evaluación", "evaluador")
+      select "entrega"."grupo", "entrega"."evaluación", "Evaluador"."nombre"
+      from
+        "Evaluador"
+          left outer join "Asignación de evaluación" on true
+            and "Evaluador"."nombre" = "Asignación de evaluación"."evaluador"
+            and "Asignación de evaluación"."evaluación" = $1
+      group by
+        "Evaluador"."nombre"
+      order by
+        count("Asignación de evaluación"."evaluador") asc
+      limit
+        1
+      ;
+    end loop;
+  end;
+$$;
 
+select "Asignar evaluación"('Etapa 1 del proyecto');
 
-
-rollback;
+-- Mostrar asignación:
+select
+  string_agg("Integrante"."estudiante", ', ') as "Grupo",
+  "Asignación de evaluación"."evaluador" as "Evaluador"
+from
+  "Grupo"
+    inner join "Asignación de evaluación" on true
+      and "Grupo"."número" = "Asignación de evaluación"."grupo"
+      and "Grupo"."evaluación" = "Asignación de evaluación"."evaluación"
+      inner join "Integrante" on true
+        and "Grupo"."número" = "Integrante"."grupo"
+        and "Grupo"."evaluación" = "Integrante"."evaluación"
+group by
+  "Grupo"."número",
+  "Grupo"."evaluación",
+  "Asignación de evaluación"."evaluador"
+order by
+  "Evaluador"
+;
